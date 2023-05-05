e1d8c04b23497c78f6871c2dd71ff2ac9d67a8c4
kamal.swami <contej.business@gmail.com>
2023-05-05 21:09:37 +0530

    added ui

--- app/page.js
+++ app/page.js
@@ -2,12 +2,23 @@ import Image from "next/image";
 import GridGame from "./components/GridGame";
 import Record from "./components/Record";
 import MyRecord from "./components/MyRecord";
+import { GiTrophy } from "react-icons/gi";
 
 export default function Home() {
   return (
     <div>
       <GridGame />
+
+      <div className="flex items-center justify-center w-full gap-3 mt-8 mb-2">
+        <GiTrophy />
+        <h2>Sapre Record</h2>
+      </div>
       <Record />
+
+      <div className="flex items-center justify-center w-full gap-3 mt-8 mb-2">
+        <GiTrophy />
+        <h2>My Record</h2>
+      </div>
       <MyRecord />
     </div>
   );
