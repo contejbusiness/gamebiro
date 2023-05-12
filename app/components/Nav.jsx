c37e507d56c4cddedd817622e6868ada950a7c5b
kamal.swami <contej.business@gmail.com>
2023-05-12 12:52:23 +0530

    updated database

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -32,7 +32,7 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="text-sm ml-2">GameBiro</p>
+        <p className="text-sm ml-2">Easy Earn</p>
       </Link>
 
       {/* Desktop Navigation */}
